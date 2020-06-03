0be8978996a576c93a9de0c3313f7a799f47b9fc
seffu <seffukioi@gmail.com>
2020-06-03 11:20:38 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(80), unique=True, nullable=False)
+    complete = db.column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
